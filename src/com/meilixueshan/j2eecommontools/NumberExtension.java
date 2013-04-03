16c2f550385aa8f1e3eae03b59cda9ee6800199a
itmanager <manager@qq.com>
2013-04-03 11:29:24 +0800

    新增NumberExtension

---
+++ src/com/meilixueshan/j2eecommontools/NumberExtension.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.meilixueshan.j2eecommontools;
+
+/**
+ *
+ * @author Administrator
+ */
+public final class NumberExtension {
+    
+}
